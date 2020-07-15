438fe244c670f51bbec153e5e8c24759a2c2c48a
Bhornumnard <tak_25908@hotmail.com>
2020-07-15 20:22:09 +0700

    update algorithm

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react'
 import './index.css'
 
 function Round(n, ops, num, arr, index) {
-    for(let i= -1; i <= n; i++){
-        console.log(n)
-        console.log(i)
-        console.log(arr[index])
-        if (ops === "x") num *= arr[index+(2*i)]
-        else if (ops === "/") num /= arr[index+(2*i)]
-        
+    for (let i = -1; i <= n; i++) {
+        if (i === -1) num = arr[index + (2 * i)]
+        else if (ops === "*") num *= arr[index + (2 * i)]
+        else if (ops === "/") num /= arr[index + (2 * i)]
+
     }
     return num
 }
@@ -16,135 +14,154 @@ function Round(n, ops, num, arr, index) {
 export default class Calculator extends Component {
     state = {
         data: [""],
-        result: 0
+        result: 0,
+        show: [],
     }
-    addNumber = (num)=>{
+    addNumber = (num) => {
         let data = [...this.state.data];
-        let lastData = data[data.length-1];
-        if(!isNaN(Number(lastData))  ){
-          [data[data.length-1]]= [data[data.length-1]+num]
-        this.setState(prevState => { return prevState = {data}})  
+        let lastData = data[data.length - 1];
+        if (!isNaN(Number(lastData))) {
+            [data[data.length - 1]] = [data[data.length - 1] + num]
+            this.setState(prevState => { return prevState = { data } })
         } else {
-            this.setState(prevState => {return prevState = {data: [...this.state.data, num]}})
+            this.setState(prevState => { return prevState = { data: [...this.state.data, num] } })
         }
-        this.setState((prevState)=> {
-            let input =  ""; 
-           prevState.data.forEach( str => input += str)
-            return prevState = {result: input}} )
+        this.setState((prevState) => {
+            let input = "";
+            prevState.data.forEach(str => input += str)
+            return prevState = { result: input }
+        })
     }
     addOps = (ops) => {
-        this.setState(prevState => prevState = {data: [...this.state.data, ops] }) 
-        
-         
-         this.setState((prevState)=> {
-            let input =  ""; 
-           prevState.data.forEach( str => input += str)
-            return prevState = {result: input}} )
+        this.setState(prevState => prevState = { data: [...this.state.data, ops] })
+
+
+        this.setState((prevState) => {
+            let input = "";
+            prevState.data.forEach(str => input += str)
+            return prevState = { result: input }
+        })
     }
-    handleResult= ()=>{
+    handleResult = () => {
         const arr = this.state.data;
-        let result = this.state.result
+        let result = this.state.result;
+        this.setState({ show: [...this.state.show, this.state.result] })
         for (let i = 0; i < arr.length - 2; i += 2) {
             if (i === 0) {
                 result = +arr[i];
             }
-            if (arr[i + 1] === "+" ) {
+            if (arr[i + 1] === "+") {
                 result += +arr[i + 2]
             }
             else if (arr[i + 1] === "-") {
                 result -= +arr[i + 2]
             }
+            // else if (arr[i + 1] === "x") {
+            //     if (arr[i - 1] === "+" || arr[i - 1] === undefined) result += +arr[i] * (+arr[i + 2]) - arr[i]
+            //     else if (arr[i - 1] === "-") result -= +arr[i] * (+arr[i + 2]) - arr[i]
+            //     let m = i
+
+            //     let num = 1, round = 0, value = 0
+            //     while (arr[m - 1] === "x" || arr[m - 1] === "/") {
+            //         round++
+            //         if (arr[m - 1] === "x") {
+            //             value = Round(round - 1, "x", num, arr, m)
+            //             num = Round(round, "x", num, arr, m)
+            //         } else if (arr[m - 1] === "/") {
+            //             value = Round(round - 1, "/", num, arr, m)
+            //             num = Round(round, '/', num, arr, m)
+            //         }
+            //         m -= 2
+            //     }
+            //     if (arr[m - 1] === "-" && value !== 0) {
+            //         result -= num - value
+            //     } else if ((arr[m - 1] === "+" || arr[m - 1] === undefined) && value !== 0) {
+            //         result += num - value
+            //     }
+
+            // }
             else if (arr[i + 1] === "x") {
-                if (arr[i - 1] === "+" || arr[i-1] === undefined) result += +arr[i] * (+arr[i + 2]) - arr[i]
-                else if (arr[i - 1] === "-") result -= +arr[i] * (+arr[i + 2]) - arr[i]
-                let m = i
-            
-                let num = 1, round = 0, value = 0
-                while (arr[m - 1] === "x" || arr[m - 1] === "/") {
-                    round++
-                    if(arr[m-1] === "x"){
-                    value = Round(round-1, "x", num, arr, m)
-                    num = Round(round, "x", num,arr,m ) 
-                    } else if (arr[m-1] === "/"){
-                        value = Round(round-1, "/", num, arr, m)
-                        num = Round (round , '/', num, arr, m) 
+                let round = 1;
+                let num = 1;
+                while (arr[i + 1] === "x" || arr[i + 1] === "/") {
+                    if (arr[i + 1] === "x") {
+                        if (round === 1) {
+                            num = +arr[i]
+                            if (arr[i - 1] === "-") result += +arr[i]
+                            else if (arr[i - 1] === "+") result -= +arr[i]
+                        } else num *= +arr[i]
+                        round++
+                        i += 2
+                    } else {
+                        num /= +arr[i]
                     }
-                     m -= 2
+
                 }
-                if(arr[m-1] === "-" && value !== 0){
-                    result -= num - value          
-                }else if ((arr[m-1] === "+" || arr[m-1] === undefined) && value !== 0){
-                    result += num - value
-                }
-        
-            }
-            else if (arr[i + 1] === "/") {
-        
-                if (arr[i - 1] === "+" || arr[i-1] === undefined) result += arr[i] / (arr[i + 2]) - arr[i]
-                else if (arr[i - 1] === "-") result -= arr[i] / (arr[i + 2]) - arr[i]
-                let m = i
-            
-                let num = 1, round = 0, value = 0
-                while (arr[m - 1] === "x" || arr[m - 1] === "/") {
-                    round++
-                    if(arr[m-1] === "x"){
-                    value = Round(round-1, "x", num, arr, m)
-                    num = Round(round, "x", num,arr,m ) 
-                    } else if (arr[m-1] === "/"){
-                        value = Round(round-1, "/", num, arr, m)
-                        num = Round (round , '/', num, arr, m) 
+                if (arr[i - (round * 2) + 1] === "+" || arr[i - (round * 2) + 1] === undefined) result += num * arr[i]
+                else if (arr[i - (round * 2) + 1] === "-") result -= num * +arr[i]
+            } else if (arr[i + 1] === "/") {
+                let round = 1;
+                let num = 1;
+                while (arr[i + 1] === "/" || arr[i + 1] === "x") {
+                    if (arr[i + 1]) {
+                        if (round === 1) {
+                            num = +arr[i];
+                            if (arr[i - 1] === "-") result += +arr[i]
+                            else if (arr[i - 1] === "+") result -= +arr[i]
+                        } else num /= +arr[i]
+                        round++
+                        i += 2
+                    } else {
+                        num *= +arr[i]
                     }
-                     m -= 2
+
                 }
-                if(arr[m-1] === "-" && value !== 0){
-                    //result /= num/ value
-                    result *= -num / value  
-                }else if((arr[m-1] === "+" || arr[m-1] === undefined) && value !== 0){
-                   // result /= num / value
-                   result *= num /value
-                    console.log(result)
-                }
-        
-        
+                if (arr[i - (round * 2) + 1] === "+" || arr[i - (round * 2) + 1] === undefined) result += num / arr[i]
+                else if (arr[i - (round * 2) + 1] === "-") result -= num / arr[i]
             }
-        
-            this.setState({result, data: [`${result}`]})
+            console.log(result)
+            this.setState({ result, data: [`${result}`] })
+
         }
     }
     render() {
         return (
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", flexDirection: "column" }}>
                 <table>
-                    <tr>
-                        <td colSpan="3"> <input value={this.state.result} /></td>
-                        <td><button onClick={() => this.setState({result : 0, data:[""]})}> c </button></td>
-                    </tr>
-                    <tr>
-                        <td><button onClick={() => this.addNumber('7')}> 7 </button></td>
-                        <td><button onClick={() => this.addNumber('8')}> 8 </button></td>
-                        <td><button onClick={() => this.addNumber('9')}> 9 </button></td>
-                        <td><button onClick={() => this.addOps("+")}> + </button></td>
-                    </tr>
-                    <tr>
-                        <td><button onClick={() => this.addNumber('4')}> 4 </button></td>
-                        <td><button onClick={() => this.addNumber('5')}> 5 </button></td>
-                        <td><button onClick={() => this.addNumber('6')}> 6 </button></td>
-                        <td><button onClick={() => this.addOps("-")}> - </button></td>
-                    </tr>
-                    <tr>
-                        <td><button onClick={() => this.addNumber('1')}> 1 </button></td>
-                        <td><button onClick={() => this.addNumber('2')}> 2 </button></td>
-                        <td><button onClick={() => this.addNumber('3')}> 3 </button></td>
-                        <td><button onClick={() => this.addOps('x')}> x </button></td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2"><button onClick={() => this.addNumber('0')}> 0 </button></td>
-                        <td><button onClick={this.handleResult}> = </button></td>
-                        <td><button onClick={() => this.addOps('/')}> % </button></td>
-                    </tr>
-
+                    <tbody>
+                        <tr>
+                            <td colSpan="3"> <input value={this.state.result} /></td>
+                            <td><button onClick={() => this.setState({ result: 0, data: [""] })}> c </button></td>
+                        </tr>
+                        <tr>
+                            <td><button onClick={() => this.addNumber('7')}> 7 </button></td>
+                            <td><button onClick={() => this.addNumber('8')}> 8 </button></td>
+                            <td><button onClick={() => this.addNumber('9')}> 9 </button></td>
+                            <td><button onClick={() => this.addOps("+")}> + </button></td>
+                        </tr>
+                        <tr>
+                            <td><button onClick={() => this.addNumber('4')}> 4 </button></td>
+                            <td><button onClick={() => this.addNumber('5')}> 5 </button></td>
+                            <td><button onClick={() => this.addNumber('6')}> 6 </button></td>
+                            <td><button onClick={() => this.addOps("-")}> - </button></td>
+                        </tr>
+                        <tr>
+                            <td><button onClick={() => this.addNumber('1')}> 1 </button></td>
+                            <td><button onClick={() => this.addNumber('2')}> 2 </button></td>
+                            <td><button onClick={() => this.addNumber('3')}> 3 </button></td>
+                            <td><button onClick={() => this.addOps('x')}> x </button></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2"><button onClick={() => this.addNumber('0')}> 0 </button></td>
+                            <td><button onClick={this.handleResult}> = </button></td>
+                            <td><button onClick={() => this.addOps('/')}> % </button></td>
+                        </tr>
+                    </tbody>
 
                 </table>
+                <ul>
+                    {this.state.show.map((list, index) => <li key={`${list}_${index}`}>{list}</li>)}
+                </ul>
             </div>
         )
     }
